da88c0a26b436b5f4a7f278ca3f0803f895377ba
Eli Murphy-Trotzky <elimtistheman@Elis-MacBook-Air.local>
2014-04-22 02:14:22 -0400

    fixed some fucker with vision and added some cool features

--- vision_main.py
+++ vision_main.py
@@ -20,17 +20,17 @@ def combine_towers(t1, t2, state):
 def get_tower_from_vision():
     savestr1 = '~/jenga/current_pics/left_cam.JPG';
     savestr2 = '~/jenga/current_pics/right_cam.JPG';
-    command_string1 = "fswebcam -D 1 -S 30 -d /dev/video1 " + savestr1;
-    command_string2 = "fswebcam -D 1 -S 30 -d /dev/video2 " + savestr2;
+    command_string1 = "fswebcam -S 40 -d /dev/video4 " + savestr1;
+    command_string2 = "fswebcam -S 40 -d /dev/video3 " + savestr2;
     os.system(command_string1);
     os.system(command_string2)
     outstr1 = '~/jenga/current_pics/left_out.JPG';
     outstr2 = '~/jenga/current_pics/right_out.JPG';
-    (t1, rowsl, colsl) = vision.main(savestr1, outstr1);
-    (t2, rowsr, colsr) = vision.main(savestr2, output2);
+    (t1, rowsl, colsl) = vision.main(savestr1, outstr1, vision.left_tower);
+    (t2, rowsr, colsr) = vision.main(savestr2, output2, vision.right_tower);
     t = combine_towers(t1, t2, True);
     t2 = (t, rowsl, colsl, rowsr, colsr)
     return t2;
 
 
-def distance_from_goal(t, r_goal, c_goal):
+#def distance_from_goal(t, r_goal, c_goal):
